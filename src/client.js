57bba3e29a496f76b53fa722e7698fbfb6b41039
Adezb <temitopeceo@gmail.com>
2022-12-29 17:42:57 +0100

    useCdn commented in clientJS

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = sanityClient({
   projectId: "uikao4q4",
   dataset: "production",
-  useCdn: false,
+  // useCdn: ,
   apiVersion: "2022-02-01",
   token:
     "skPulaXfwrgrT3ab17Fny2Mykf9lmXdTTaatB6hVX70Ub37YrRsRvq828L0rwq2gxuoaXFbWJrYgb5h8cdMAGvAz9pvXulzIj1oZXCDbg7td0Tj6eFTjq6hHvKPYZatfzJrhniKOw1FGYxTWkMVeSYszglrjhfoanhprkSc7wL5GlTobNaxe",
