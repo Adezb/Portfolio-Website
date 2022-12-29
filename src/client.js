27cb25b5c5d335dd97e1227e8de9eb2a7f08938e
Adezb <temitopeceo@gmail.com>
2022-12-29 16:24:12 +0100

    clientJS updated and env file deleted

--- src/client.js
+++ src/client.js
@@ -2,11 +2,12 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: "uikao4q4",
   dataset: "production",
   useCdn: true,
   apiVersion: "2022-02-01",
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token:
+    "skPulaXfwrgrT3ab17Fny2Mykf9lmXdTTaatB6hVX70Ub37YrRsRvq828L0rwq2gxuoaXFbWJrYgb5h8cdMAGvAz9pvXulzIj1oZXCDbg7td0Tj6eFTjq6hHvKPYZatfzJrhniKOw1FGYxTWkMVeSYszglrjhfoanhprkSc7wL5GlTobNaxe",
 });
 
 const builder = imageUrlBuilder(client);
